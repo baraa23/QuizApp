e2437ea954fdcdef0244955d5649c7c1612dd926
baraa23 <baraa.salaymeh1@gmail.com>
2018-02-04 23:39:39 +0100

    update

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.quizapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,13 @@ import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * with this app you can test your general knowledge
+     * (made as the final project for Udacity/Google Challenge Scholarship: Android Basics)
+     */
+
+    public static final int MAX_SCORE = 4;
 
 
     @Override
@@ -23,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when submit button is clicked.
      */
 
-    public void sumbitAnswers (View view) {
+    public void submitAnswers(View view) {
 
         int score = 0;
 
@@ -33,8 +41,14 @@ public class MainActivity extends AppCompatActivity {
         RadioButton burj = (RadioButton) findViewById(R.id.q2_a);
         boolean question2a = burj.isChecked();
 
+        CheckBox china = (CheckBox) findViewById(R.id.q3_a);
+        boolean question3a = china.isChecked();
+
         CheckBox egypt = (CheckBox) findViewById(R.id.q3_b);
         boolean question3b = egypt.isChecked();
+
+        CheckBox lebanon = (CheckBox) findViewById(R.id.q3_c);
+        boolean question3c = lebanon.isChecked();
 
         CheckBox sudan = (CheckBox) findViewById(R.id.q3_d);
         boolean question3d = sudan.isChecked();
@@ -45,20 +59,39 @@ public class MainActivity extends AppCompatActivity {
         EditText yourName = (EditText) findViewById(R.id.user_name);
         String editTextName = yourName.getText().toString();
 
-        score = calculateScore(question1b,question2a,question3b,question3d,question4d);
+        score = calculateScore(question1b, question2a, question3a, question3b, question3c, question3d, question4d);
+
+        Context context = getApplicationContext();
+        CharSequence text = getString(R.string.congrats) + editTextName + getString(R.string.scoremessagestart)
+                + score + getString(R.string.scoremesaaageend) + MAX_SCORE;
+
+        Context contextwrong = getApplication();
+        CharSequence textwrong = getString(R.string.messagewrongstart) + editTextName + getString(R.string.messagewrongstart2)
+                + score + getString(R.string.messagewrongmid) + MAX_SCORE + getString(R.string.messagewrongend);
+
 
         if (score >= 4) {
-            Toast.makeText(this, "your result is: " + score + "out of 4", Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, text, Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(contextwrong, textwrong, Toast.LENGTH_LONG).show();
         }
-        else {
-            Toast.makeText(this, "sorry, try again, you have score" + score + "out of 4", Toast.LENGTH_SHORT).show();
-        }
-
-
 
     }
 
-    private int calculateScore (boolean ques1, boolean ques2, boolean ques3b, boolean ques3d, boolean ques4d) {
+    /**
+     * this method to calculate the score and return it
+     *
+     * @param ques1  right answer for question 1 radiobutton
+     * @param ques2  right answer for question 2 radiobutton
+     * @param ques3a wrong answer for question 3 checkbox
+     * @param ques3b right answer for question 3 checkbox
+     * @param ques3c wrong answer for question 3 checkbox
+     * @param ques3d right answer for question 3 checkbox
+     * @param ques4d right answer for question 4 radiobutton
+     * @return return the score
+     */
+
+    private int calculateScore(boolean ques1, boolean ques2, boolean ques3a, boolean ques3b, boolean ques3c, boolean ques3d, boolean ques4d) {
         int result = 0;
 
         if (ques1) {
@@ -67,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
         if (ques2) {
             result++;
         }
-        if (ques3b && ques3d) {
+        if (ques3b && ques3d && !ques3a && !ques3c) {
             result++;
         }
 
@@ -79,7 +112,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void resetQuiz (View view) {
+    /**
+     * this method to reset the app and clear all checkboxes, radio groups, and editText.
+     *
+     * @param view
+     */
+
+    public void resetQuiz(View view) {
 
         //clear edit text
         EditText username = (EditText) findViewById(R.id.user_name);
@@ -109,7 +148,6 @@ public class MainActivity extends AppCompatActivity {
         checkbox4.setChecked(false);
 
     }
-
 
 
 }
